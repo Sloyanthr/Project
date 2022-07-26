cee8edc81a9ffd26a8f70e80c3ae03a726e0a82f
Sloyanthr <104297950+Sloyanthr@users.noreply.github.com>
2022-07-27 00:53:11 +0800

    Add files via upload

---
+++ SM3.cpp
@@ -0,0 +1,162 @@
+#include<iostream>
+#include<chrono>
+using namespace std;
+/*
+const int MESS[] = { 0x61,0x62,0x63 };	//每个元素一字节
+*/
+const int MESS[] = {0x61,0x62,0x63,0x64,0x61,0x62,0x63,0x64,0x61,0x62,0x63,0x64,0x61,0x62,0x63,0x64,
+					0x61,0x62,0x63,0x64,0x61,0x62,0x63,0x64,0x61,0x62,0x63,0x64,0x61,0x62,0x63,0x64,
+					0x61,0x62,0x63,0x64,0x61,0x62,0x63,0x64,0x61,0x62,0x63,0x64,0x61,0x62,0x63,0x64,
+					0x61,0x62,0x63,0x64,0x61,0x62,0x63,0x64,0x61,0x62,0x63,0x64,0x61,0x62,0x63,0x64, };	//每个元素一字节
+
+#define ll long long
+#define a 0
+#define b 1
+#define c 2
+#define d 3
+#define e 4
+#define f 5
+#define g 6
+#define h 7
+
+const unsigned int IV[8] =
+{
+	0x7380166f,	0x4914b2b9,
+	0x172442d7,	0xda8a0600,
+	0xa96f30bc,	0x163138aa,
+	0xe38dee4d,	0xb0fb0e4e,
+};
+
+unsigned int T[64] = { 0 };
+//初始化T，每次运行SM3前需调用
+void _init_T()
+{
+	int i = 0;
+	for (; i < 16; i++)
+		T[i] = 0x79CC4519;
+	for (; i < 64; i++)
+		T[i] = 0x7A879D8A;
+}
+//循环左移，对int类型变量X循环左移n位
+int ROTL(int X, int n) {
+	return (((X << n) & 0xffffffff) | ((X & 0xffffffff) >> (32 - n)));
+}
+//布尔函数
+int FF(int X, int Y, int Z, int j) {
+	if (j < 16 && j >= 0)
+		return(X ^ Y ^ Z);
+	if (j > 15 && j < 64)
+		return((X & Y) | (X & Z) | (Y & Z));
+	return false;
+}
+int GG(int X, int Y, int Z, int j) {
+	if (j < 16 && j >= 0)
+		return(X ^ Y ^ Z);
+	if (j > 15 && j < 64)
+		return ((X & Y) | ((~X) & Z));
+	return false;
+}
+//压缩函数中的置换函数
+int P0(int X) {
+	return X ^ ROTL(X, 9) ^ ROTL(X, 17);
+}
+//消息扩展中的置换函数
+int P1(int X) {
+	return X ^ ROTL(X, 15) ^ ROTL(X, 23);
+}
+//压缩函数
+void CF(int* v,int* B) {
+	int W68[68] = { 0 };
+	int W64[64] = { 0 };
+	int V[8] = { 0 };
+	for (int i = 0; i < 8; i++)
+		V[i] = v[i];
+	//消息拓展
+	int j = 0;
+	for (; j < 16; j++)
+		W68[j] = B[j];
+	for (; j <= 67; j++)
+		W68[j] = P1(W68[j - 16] ^ W68[j - 9] ^ ROTL(W68[j - 3], 15)) ^ ROTL(W68[j - 13], 7) ^ W68[j - 6];
+	for (j = 0; j < 64; j++)
+		W64[j] = W68[j] ^ W68[j + 4];
+	//压缩函数
+	int SS1 = 0, SS2 = 0, TT1 = 0, TT2 = 0;
+	for (j = 0; j < 64; j++) {
+		SS1 = ROTL(ROTL(V[a], 12) + V[e] + ROTL(T[j], j), 7);
+		SS2 = SS1 ^ ROTL(V[a], 12);
+		TT1 = FF(V[a], V[b], V[c], j) + V[d] + SS2 + W64[j];
+		TT2 = GG(V[e], V[f], V[g], j) + V[h] + SS1 + W68[j];
+		V[d] = V[c];
+		V[c] = ROTL(V[b], 9);
+		V[b] = V[a];
+		V[a] = TT1;
+		V[h] = V[g];
+		V[g] = ROTL(V[f], 19);
+		V[f] = V[e];
+		V[e] = P0(TT2);
+	}
+	for (int i = 0; i < 8; i++)
+		v[i] ^= V[i];
+}
+//迭代过程，B为填充后的分组，共 512*n bit
+void iterate(int* B, int* V, int n) {
+	for (int i = 0; i < 8; i++)
+		V[i] = IV[i];
+	for (int i = 0; i < n; i++) 
+		CF(V, (B + (i * 16)));
+}
+//哈希函数，输入输出均为每个元素4字节的int类型数组，size为字节数
+void SM3(int* input, int* output,ll size) {
+	_init_T();
+	//填充
+	ll n = size / 64;
+	ll k = size % 64;
+	size *= 8;	//总bit数
+	//512bit，即16个字为一组B[i]，一共16 * (n + 1)个元素，即n+1组
+	int* B = new int[16 * (n + 1)];
+	B[16 * n + 15] = size;
+	B[16 * n + 14] = size >> 32;
+	int i = 0;
+	for (; i < 16 * n + k / 4; i++)
+		B[i] = input[i];
+	if (k % 4 == 0)
+		B[i] = 0x80000000;
+	else
+		B[i] = input[i] | (0x80 << ((3 - (k % 4)) * 8));
+	++i;
+	for (; i < 16 * n  + 14; i++)
+		B[i] = 0;
+	n++;
+	iterate(B, output, n);
+}
+void test() {
+	//首先把一个元素一个字节的数组MESS(字符串)，变为一个元素一个字的数组B(int)
+	ll size = sizeof(MESS) / sizeof(int);	//size为所占的字节数
+	int V[8] = { 0 };
+	//int B[1] = { 0x616263 };
+	//int B[1] = { 0x616263 };
+	
+	int* B = new int[size / 4 + (bool)(size % 4)];
+	int i = 0;
+	for (; i < size / 4; i++)
+		B[i] = MESS[i * 4] << 24 | MESS[i * 4 + 1] << 16 | MESS[i * 4 + 2] << 8 | MESS[i * 4 + 3];
+	if (size % 4) {
+		B[i] = 0;
+		for (int k = 0; k < size % 4; k++)
+			B[i] = B[i] | (MESS[i * 4 + k] << (8 * (3 - k)));
+	}
+	/*
+	for (int i = 0; i < size / 4 + (bool)(size % 4); i++)
+		cout << hex << B[i] << endl;
+	*/
+	//然后将B作为SM3的输入
+	SM3(B, V, size);
+	cout << hex ;
+	for (int i = 0; i < 8; i++)
+		cout << V[i] << ' ';
+	
+}
+int main() {
+	test();
+	return 0;
+}
