5f8679d0637ad827b30b9a5c2dc2d8d3f5c4a19e
Sloyanthr <104297950+Sloyanthr@users.noreply.github.com>
2022-07-30 00:05:44 +0800

    Update SM3 rho attack.py

--- SM3 rho attack.py
+++ SM3 rho attack.py
@@ -3,9 +3,9 @@ from gmssl import sm3, func
 
 #n为攻击长度，mess为初始信息
 def rho(n,mess):
+  flag = 0
   lis = []
   lis.append(sm3.sm3_hash(func.bytes_to_list(mess))[:n])
-  flag = 0
   start = time()
   while(1):
     data = bytes(lis[-1], encoding='utf-8')
@@ -13,11 +13,11 @@ def rho(n,mess):
     data = bytes(x, encoding='utf-8')
     t_x = sm3.sm3_hash(func.bytes_to_list(data)[:n])
     for j in lis:
-        if t_x==j:
-            flag=1
+        if t_x == j:
             print(lis.index(j),"\n碰撞哈希:\n", j, "\n元素号:\n", len(lis), '\n',t_x)
+            flag=1
             break
-    if flag==1:
+    if flag == 1:
         break
     lis.append(t_x)
   end = time()
