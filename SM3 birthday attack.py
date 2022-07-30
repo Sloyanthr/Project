4149408db684d3c7926d4c508f77f64ebdcaef4b
Sloyanthr <104297950+Sloyanthr@users.noreply.github.com>
2022-07-30 19:38:48 +0800

    Update SM3 birthday attack.py

--- SM3 birthday attack.py
+++ SM3 birthday attack.py
@@ -5,15 +5,8 @@ import binascii
 from gmssl import sm3, func
 alphatable = "abcdefghijklmnopqrstuvwxyz"
 
-def randomstr(randomlength=8):
-  random_str =''
-  length = len(alphatable) -1
-  for i in range(randomlength):
-    random_str += alphatable[random.randint(0, length)]
-  return random_str
 #查找并加入,num为攻击次数，length为字符串长度
-
-def attack(num,length = 6):
+def attack(num,n,length = 6):
     hashmap={}
     i = 0
     for s in itertools.permutations(alphatable,length):
@@ -23,7 +16,7 @@ def attack(num,length = 6):
             strs+=s[k]
         data = bytes(strs, encoding='utf-8')
         #切片大小界定碰撞长度
-        sign = sm3.sm3_hash(func.bytes_to_list(data))[:7]
+        sign = sm3.sm3_hash(func.bytes_to_list(data))[:n]
         if sign in hashmap:
           print("攻击成功")
           return (hashmap[sign],strs)
@@ -35,4 +28,4 @@ def attack(num,length = 6):
     return (0, 0)
 
 
-print(attack(pow(2,16),8))
+print(attack(pow(2,16),7,8))
